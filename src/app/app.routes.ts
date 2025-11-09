004fe3671242508024299cc3829e27817c8416b9
Ravish.Varma <ravish2000kumar@gmail.com>
2025-11-09 13:24:59 +0530

    page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Feed } from './components/feed/feed';
 import { NotFoundError } from 'rxjs';
 import { loginGuard, authGuard } from './guards/login-guard';
 import { Privacy } from './components/policy/privacy/privacy';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -32,6 +33,10 @@ export const routes: Routes = [
                 path: "connections",
                 loadComponent: () => import("./components/connections/connections").then(M => M.Connections)
             },
+            {
+                path: "chat/:userId",
+                loadComponent: () => import("./components/chat/chat").then(M=> M.Chat)
+            }
 
         ]
     },
@@ -40,7 +45,11 @@ export const routes: Routes = [
         loadChildren: () => import("./components/policy/policy-module").then(M => M.PolicyModule)
     },
     {
+        path: "page-not-found",
+        component: PageNotFoundComponent
+    },
+    {
         path: "**",
-        component: NotFoundError
+        redirectTo: "page-not-found"
     }
 ];
---
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "page-not-found",
+    imports: [],
+    templateUrl: "./page-not-found.component.html"
+
+})
+
+export class PageNotFoundComponent {
+
+}
